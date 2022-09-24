266246295d6313ccd19d864b63a06e8752812d18
Javier Delgado <javiercambranis23@gmail.com>
2022-09-24 13:16:37 -0500

    Curso de Nest

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { BrandsService } from 'src/brands/brands.service';
+import { CarsService } from 'src/cars/cars.service';
+import { BRANDS_SEED } from './data/brands.seed';
+import { CARS_SEED } from './data/cars.seed';
+
+@Injectable()
+export class SeedService {
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandService: BrandsService,
+  ) {}
+  populateDB() {
+    // CARS_SEED
+    // BRANDS_SEED
+    this.carsService.fillCarsWithSeedData(CARS_SEED);
+    this.brandService.fillBrandsWithSeedData(BRANDS_SEED);
+    return 'Seed executed successfully';
+  }
+}
